3ceeb0a9dc3d6af1b51982f680f2c80b7c51cf9e
Krishi Shah <166585009+Krishishah7@users.noreply.github.com>
2026-04-15 09:45:20 +0530

    Added UI files for RAG System

---
+++ 06_llm_and_fine_tuning/32_rag_streamlit_ui/app_ui.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import requests
+
+# Title
+st.title("RAG AI Assistant 🤖")
+
+# Input box
+query = st.text_input("Enter your question:")
+
+# Button click
+if st.button("Ask"):
+    if not query.strip():
+        st.warning("Please enter a question.")
+    else:
+        try:
+            response = requests.post(
+                "http://127.0.0.1:8000/ask",
+                json={"query": query}
+            )
+            
+            data = response.json()
+            
+            # Display output
+            st.subheader("Answer")
+            st.write(data["answer"])
+            
+            st.subheader("Confidence")
+            st.write(data["confidence"])
+            
+            # Optional fields
+            if data.get("tool_output"):
+                st.subheader("Tool Output")
+                st.write(data["tool_output"])
+                
+        except Exception as e:
+            st.error("Error connecting to API. Make sure FastAPI server is running.")
